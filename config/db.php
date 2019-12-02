bb09539a96d76bc24bfa6bcd6525d1b5934e6907
Maxcxam <maxcxam1979@gmail.com>
2019-12-02 14:17:36 +0200

    db config

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yuii',
+    'username' => 'yuii',
+    'password' => 'yuiiyuii',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
